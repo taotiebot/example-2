f4271d639df21274262ed2cf9a554545bc827cb5
murshidazher <hello@murshidazher.com>
2023-08-03 20:18:24 +0530

    feat: add initial files

---
+++ src/common.ts
@@ -0,0 +1,14 @@
+// Some typescript symbols
+export const featureFlag = 'featureFlag'
+
+export function f1() {
+  return 'f1'
+}
+
+export function f2() {
+  return 'f2'
+}
+
+export function f3() {
+  return 'f2'
+}
